205eca38699b8947f3859faaabaffd2e50ac1001
lfrancois <liesbeth.francois@ucb.com>
2018-06-07 13:53:10 +0200

    Update parsing of source files

--- scripts/S02-Process-Sources.R
+++ scripts/S02-Process-Sources.R
@@ -43,38 +43,124 @@ MedGen_conso$SAB[MedGen_conso$SAB == "NCI"] <- "NCIt"
 MedGen_conso$SAB[MedGen_conso$SAB == "SNOMEDCT_US"] <- "SNOMEDCT"
 MedGen_conso$SAB[MedGen_conso$SAB == "ORDO"] <- "ORPHA"
 MedGen_conso$DB <- "MedGen"
+MedGen_conso$SDUI <- gsub(".*:","",MedGen_conso$SDUI)
+MedGen_conso$SDUI <- gsub(".*_","",MedGen_conso$SDUI)
+MedGen_conso$id <- ifelse(MedGen_conso$SAB %in% c("NCIt","SNOMEDCT"),MedGen_conso$SCUI,MedGen_conso$SDUI)
+MedGen_conso <- MedGen_conso[-which(MedGen_conso$SAB == ""),]
 
-MedGen_crossId <- MedGen_conso[,c("DB","X.CUI","SAB","SDUI")]
+MedGen_crossId <- unique(MedGen_conso[,c("DB","X.CUI","SAB","id")])
 names(MedGen_crossId) <- c("DB1","id1","DB2","id2")
+table(MedGen_crossId$DB1)
+table(MedGen_crossId$DB2)
+dim(MedGen_crossId)
+
+## When removing prefix, an integer is a correct disease ID
+table(!is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id2))))
+table(!is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id1))))
+toKeep <- MedGen_crossId[which(!is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id2))) &
+                                !is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id1)))),]
+dim(toKeep)
+toCheck <- MedGen_crossId[-which(!is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id2))) &
+                            !is.na(as.numeric(sub("^[^[:digit:]]*", "", MedGen_crossId$id1)))),]
+dim(toCheck)
+
+## Remove any DBs that are not disease DBs and DB1 can only be "EFO" or "Orphanet"
+## check wrong IDs, remove weird ones still
+table(toKeep$DB2)
+table(toKeep$DB1)
+
+## Remove self references
+MedGen_crossId[which(MedGen_crossId$id1 == MedGen_crossId$id2),]
+## MedGen_crossId <- MedGen_crossId[-which(MedGen_crossId$id1 == MedGen_crossId$id2),]
+
+## Keep only disease
+MedGen_dis <- read.table(file.path(sdir,"MGSTY.RFF"), sep = "|", header = TRUE, comment.char = "", quote = "", 
+                         fill = TRUE, colClasses = c("character"))
+MedGen_dis <- MedGen_dis[grep(paste("Disease or Syndrome","Acquired Abnormality",
+                                    "Anatomical Abnormality","Congenital Abnormality",sep = "|"),
+                              MedGen_dis$STY),]
+MedGen_crossId <- MedGen_crossId[which(MedGen_crossId$id1 %in% MedGen_dis$X.CUI),]
+dim(MedGen_crossId)
 
 ## idNames
 MedGen_idNames <- read.table(file.path(sdir,"NAMES.RGG"), sep = "|", header = TRUE, comment.char = "", quote = "", 
                              fill = TRUE, colClasses = c("character"))
 MedGen_idNames$DB <- "MedGen"
 MedGen_idNames <- MedGen_idNames[,c("DB","X.CUI","name")]
-names(MedGen_idNames) <- c("DB","id","name")
-MedGen_idNames$canonical <- TRUE
-MedGen_idNames$name <- tolower(MedGen_idNames$name)
-MedGen_idNames$name <- gsub("[[:punct:]]"," ",MedGen_idNames$name)
-MedGen_idNames$name <- iconv(x = MedGen_idNames$name,to="ASCII//TRANSLIT")
-MedGen_idNames$name <- gsub("\n"," ",MedGen_idNames$name)
+names(MedGen_idNames) <- c("DB","id","syn")
+## * Finding non-numeric ID 
+id <-  (MedGen_idNames$id)
+numidsuf <- as.numeric(sub("^[^[:digit:]]*", "", id))
+MedGen_idNames[which(is.na(numidsuf)),]
+MedGen_idNames <- MedGen_idNames[-which(is.na(numidsuf)),] 
+MedGen_idNames$canonical <- ifelse(MedGen_idNames$syn %in% MedGen_conso$STR,TRUE, FALSE)
+
+## Check characters for \t, \n, \r and put to ASCII
+MedGen_idNames$syn <- iconv(x = MedGen_idNames$syn,to="ASCII//TRANSLIT")
+table(unlist(sapply(MedGen_idNames$syn, strsplit, split = "")))
+MedGen_idNames$syn <- gsub(paste("\n","\t","\r", sep = "|")," ",MedGen_idNames$syn)
+table(unlist(sapply(MedGen_idNames$syn, strsplit, split = "")))
+MedGen_idNames$syn <- gsub("\"","'",MedGen_idNames$syn)
+table(unlist(sapply(MedGen_idNames$syn, strsplit, split = "")))
+
+## Remove NA
+table(is.na(MedGen_idNames$syn))
+MedGen_idNames <- MedGen_idNames[!is.na(MedGen_idNames$syn),]
+## Remove duplicated (keep canonical)
+dim(MedGen_idNames)
+dim(unique(MedGen_idNames))
+MedGen_idNames <- MedGen_idNames[order(MedGen_idNames$canonical,decreasing = T),]
 MedGen_idNames <- unique(MedGen_idNames)
+dim(MedGen_idNames)
+
+## all idNames in entryId
+table(MedGen_idNames$id %in% MedGen_dis$X.CUI)
+MedGen_idNames <- MedGen_idNames[which(MedGen_idNames$id %in% MedGen_dis$X.CUI),]
+
+## Remove empty names, ifany
+nc <- nchar(MedGen_idNames$syn)
+head(table(nc))
+MedGen_idNames[which(nc < 3),]
+## Remove names of 0 or 1 character long
+MedGen_idNames[which(nc == 0),]
+MedGen_idNames[which(nc == 1),]
+MedGen_idNames <- MedGen_idNames[-which(nc == 1),]
 
 ## entryId
-MedGen_entryId <- unique(data.frame(DB = c(MedGen_conso$DB,MedGen_idNames$DB),
-                             id = c(MedGen_conso$X.CUI,MedGen_idNames$id),
+MedGen_entryId <- unique(data.frame(DB = c(MedGen_crossId$DB1,MedGen_idNames$DB),
+                             id = c(MedGen_crossId$id1,MedGen_idNames$id),
                              stringsAsFactors = F))
 MedGen_def <- read.table(file.path(sdir,"MGDEF.RFF"), sep = "|", header = TRUE, comment.char = "", quote = "", 
                            fill = TRUE, colClasses = c("character"))
-MedGen_entryId$definition <- MedGen_def$DEF[match(MedGen_entryId$id, MedGen_def$X.CUI)]
-MedGen_entryId$definition <- ifelse(MedGen_entryId$definition == "NA",NA,MedGen_entryId$definition)
-MedGen_entryId$definition <- tolower(MedGen_entryId$definition)
-MedGen_entryId$definition <- gsub("[[:punct:]]"," ",MedGen_entryId$definition)
-MedGen_entryId$definition <- iconv(x = MedGen_entryId$definition,to="ASCII//TRANSLIT")
-MedGen_entryId$definition <- gsub("\n"," ",MedGen_entryId$definition)
+MedGen_entryId$def <- MedGen_def$DEF[match(MedGen_entryId$id, MedGen_def$X.CUI)]
+## Check characters for \t, \n, \r and put to ASCII
+MedGen_entryId$def <- iconv(x = MedGen_entryId$def,to="ASCII//TRANSLIT")
+table(unlist(sapply(MedGen_entryId$def, strsplit, split = "")))
+MedGen_entryId$def <- gsub(paste("\n","\t","\r", sep = "|")," ",MedGen_entryId$def)
+table(unlist(sapply(MedGen_entryId$def, strsplit, split = "")))
+MedGen_entryId$def <- gsub("\"","'",MedGen_entryId$def)
+MedGen_entryId$def <- gsub("\\\\","",MedGen_entryId$def)
+table(unlist(sapply(MedGen_entryId$def, strsplit, split = "")))
 
+## check NA
+table(is.na(MedGen_entryId$def))
+## Remove duplicated (keep canonical)
+dim(MedGen_entryId)
+dim(unique(MedGen_entryId))
 
-rm(MedGen_conso, MedGen_def)
+## all idNames in entryId
+table(MedGen_entryId$id %in% MedGen_dis$X.CUI)
+# MedGen_entryId <- MedGen_entryId[which(MedGen_entryId$id %in% MedGen_dis$X.CUI),]
+## Remove empty names, ifany
+nc <- nchar(MedGen_entryId$def)
+head(table(nc))
+MedGen_entryId[which(nc < 3),]
+## Remove names of 0 or 1 character long
+MedGen_entryId[which(nc == 0),]
+MedGen_entryId[which(nc == 1),]
+MedGen_entryId$def[which(nc < 3)] <- NA
+
+rm(MedGen_conso, MedGen_def, MedGen_dis)
 
 
 ###############################################################################@
@@ -94,7 +180,7 @@ for(f in toSave){
   write.table(
     get(f),
     file=file.path(ddir, paste(f, ".txt", sep="")),
-    sep="|",
+    sep="\t",
     row.names=FALSE, col.names=TRUE,
     quote=FALSE
   )
